85d1a30d8fcea08e3bb587cf8e20ded5a6fb6718
djennewe <Doug.Jennewein@usd.edu>
2017-02-01 17:07:49 -0600

    seems to work now

--- pm3.py
+++ pm3.py
@@ -10,12 +10,12 @@ from Bio import SeqIO
 
 # I manually removed the first line of the input file as it contains column definitions
 #input_file_name = "sample-input.txt"
-input_file_name = "test-input.txt" # Real input
+input_file_name = "input.txt" # Real input
 
 # Store all mouse protein records in a Dict indexed on gi number
-#mice = {}
-#for seq_record in SeqIO.parse("mouse.fasta", "fasta"):
-#		mice[seq_record.id.split('|')[1]] = seq_record
+mice = {}
+for seq_record in SeqIO.parse("mouse.fasta", "fasta"):
+		mice[seq_record.id.split('|')[1]] = seq_record
 
 outfile = open("test-output.fasta","w")
 
@@ -23,11 +23,8 @@ with open(input_file_name, "r") as infile:
 	for line in infile:
 		if line.startswith("SeqID"):
 			# new record
-			gi = line.split('|')[0].split(' ')[1]
+			#gi = line.split('|')[0].split(' ')[1]
+			gi = line.split('|')[1]
 		elif len(line.split()) > 2 and line.split()[0] == "Extracellular" and line.split()[2] == "*":# split on whitespace
-			print gi
-			
-	
-
-
-#			SeqIO.write(mice[gi], outfile, "fasta")
+			#print gi
+			SeqIO.write(mice[gi], outfile, "fasta")
