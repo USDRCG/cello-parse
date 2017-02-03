e37d14c5d87f758e91375b9cc103b65252acabd3
djennewe <Doug.Jennewein@usd.edu>
2017-02-03 10:34:57 -0600

    refactored

--- cello-parse.py
+++ cello-parse.py
@@ -8,23 +8,28 @@
 
 from Bio import SeqIO
 
-# I manually removed the first line of the input file as it contains column definitions
 #input_file_name = "sample-input.txt"
-input_file_name = "input.txt" # Real input
+input_file_name = "full-input.txt" # Real input
 
 # Store all mouse protein records in a Dict indexed on gi number
 mice = {}
 for seq_record in SeqIO.parse("mouse.fasta", "fasta"):
 		mice[seq_record.id.split('|')[1]] = seq_record
 
-outfile = open("test-output.fasta","w")
+outfile = open("output.fasta","w")
 
 with open(input_file_name, "r") as infile:
 	for line in infile:
 		if line.startswith("SeqID"):
-			# new record
-			#gi = line.split('|')[0].split(' ')[1]
+			# new record, store its GI number, but only keep if it's "Extracellular"
 			gi = line.split('|')[1]
-		elif len(line.split()) > 2 and line.split()[0] == "Extracellular" and line.split()[2] == "*":# split on whitespace
-			#print gi
-			SeqIO.write(mice[gi], outfile, "fasta")
+		else:
+			# lines we want look like this:
+			#  	    Extracellular	    2.632  *
+			# three white-space-delimited columns, with "Extracellular" in the first 
+			# column, and "*" in the third, last column
+			cols = line.split()	
+			if len(cols) > 2 and cols[0] == "Extracellular" and cols[2] == "*":
+				# Print FASTA records only for those GI's whose CELLO records are 
+				# "Extracellular"
+				SeqIO.write(mice[gi], outfile, "fasta")
